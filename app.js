8922fd829969452465bb86941f0661fb4f9ac3a4
Isac Mamede <isac.mamede14@gmail.com>
2023-07-20 11:25:26 -0300

    working

---
+++ app.js
@@ -0,0 +1,28 @@
+const months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+]
+
+const weekdays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+]
+
+const renew = document.querySelector('.renew')
+const deadline = document.querySelector('deadline')
+const items = document.querySelector('deadline-format h4')
